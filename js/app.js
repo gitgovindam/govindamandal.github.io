9fa674d7f10d5aae76a5f79f938a8052043b2703
Govinda Mandal <govinda4india@gmail.com>
2020-12-06 08:08:20 +0530

    Completed the project

--- js/app.js
+++ js/app.js
@@ -17,12 +17,92 @@
     const counters = document.querySelectorAll('.counter');
 
     runCounter = () => {
-        counters.forEach(el => {
-            el.innerHTML = 0;
-            let target = +el.dataset.count;
-            console.log(target);
+        counters.forEach(counter => {
+            counter.innerHTML = 0;
+            let target = +counter.dataset.count;
+            
+            let step = target/100;
+            let countIt = function() {
+                let displayedCount = +counter.innerHTML;
+
+                if (displayedCount < target) {
+                    counter.innerHTML = displayedCount + Math.ceil(step);                    
+                    setInterval(() => {
+                        countIt()
+                    }, 10);
+                } else {
+                    counter.innerHTML = target;
+                }
+            }
+
+            countIt();
         })
     };
 
-    runCounter();
+    let intersected = 0;
+
+    let counter_section = document.querySelector('.counter-section');
+    let config = {
+        rootMargin: '0px 0px -180px 0px'
+    };
+    const section_observer = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && intersected !== 1) {
+            runCounter();
+            intersected = 1;
+        }
+    }, config);
+
+    section_observer.observe(counter_section);
+
+    // Image filter
+    var $wrraper = $('.portfolio-wrapper');
+
+    // Initialize images
+    $wrraper.isotope({
+        filter: '*',
+        layoutMode: 'masonry',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear'
+        }
+    });
+
+    let tabs = document.querySelectorAll('.tabs a');
+    tabs.forEach(link => {
+        let selector = link.dataset.filter;
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            $wrraper.isotope({
+                filter: selector,
+                layoutMode: 'masonry',
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear'
+                }
+            });
+            
+            tabs.forEach(el => {
+                el.classList.remove('active');
+            });
+
+            link.classList.add('active');
+        });
+    });
+
+    $('.magnify').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: true
+        }
+    });
+
+    // Slider
+    $('.slider').slick({
+        arrows: false,
+        autoplay: true
+    });
+
 })();
